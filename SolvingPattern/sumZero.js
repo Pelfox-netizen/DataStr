d5472d005c961eb04ac7387791e207ba49a0dfca
Max <sveramaxim@gmail.com>
2021-07-22 11:54:09 -0500

    Sum Zero

---
+++ SolvingPattern/sumZero.js
@@ -0,0 +1,37 @@
+/* 
+Write a function called sumZero which accepts a sorted array of integers.The function should find the first pair where the sum is 0;
+Return an arrray that includes both values that sum to zero or undefined if a pair does not exists.
+*/
+
+// Naive solution
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(sumZero([4, 7, 3, -3, -2, -1]));
+
+// Non naive solution
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+//console.log(sumZero([4, 7, 3, -3, -2, -1]));
