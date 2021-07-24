f4a9667c951ba4692583351520fd520ad4a75de0
Max <sveramaxim@gmail.com>
2021-07-24 10:36:01 -0500

    linear Search

---
+++ search/linearSearch.js
@@ -0,0 +1,13 @@
+function LinSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// console.log(LinSearch([1, 23, 5, 6], 6));
+
+
+
