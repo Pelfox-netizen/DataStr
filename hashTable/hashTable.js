7a2fe5a957c0cbc021e1d11c09cc350339a3f457
Max <sveramaxim@gmail.com>
2021-08-05 19:00:44 -0500

    Hash function

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -1,10 +1,18 @@
 class HashTable {
-constructor () {
+  constructor() {
+    this.table = new Array(11);
     this.size = 0;
+  }
+
+  // hash function
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash;
+  }
 }
 
-// hash function
-
-
-
-}
+const hashTable = new HashTable();
+console.log(hashTable);
